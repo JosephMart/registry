ab3a15f4a902e86550dfb0aea599fceedaf94523
Joseph Martinsen <joseph@martinsen.com>
2020-04-05 00:19:16 -0500

    db magic

---
+++ api/register.ts
@@ -0,0 +1,18 @@
+import { NowRequest, NowResponse } from "@now/node";
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import { User } from "../src/entity/User";
+
+export default async (req: NowRequest, res: NowResponse): Promise<void> => {
+  const connection = await createConnection({
+    type: "mongodb",
+    entities: [User],
+    url: process.env.MONGODB_URL,
+  });
+
+  console.log("Loading users from the database...");
+  const users = await connection.manager.find(User);
+  console.log("Loaded users: ", users);
+  await connection.close();
+  res.json({ response: "Pong!", users });
+};
