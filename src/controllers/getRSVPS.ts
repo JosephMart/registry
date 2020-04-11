7ef84d32d1d3ae40985d88121cf7549aa60f3627
Joseph Martinsen <joseph@martinsen.com>
2020-04-11 14:53:07 -0500

    Actually do last commit :P

--- src/controllers/getRSVPS.ts
+++ src/controllers/getRSVPS.ts
@@ -6,6 +6,5 @@ export default async (connection: Connection): Promise<RSVP[]> => {
   console.log("Loading RSVP from the database...");
   const rsvps = await connection.manager.find(RSVP);
   console.log("Loaded RSVPS: ", rsvps);
-  await connection.close();
   return rsvps;
 };
