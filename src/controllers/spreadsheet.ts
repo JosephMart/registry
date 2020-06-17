18bfcfb6903746588841f8e7c3d1ba296258c86e
Joseph Martinsen <joseph@martinsen.com>
2020-06-16 22:56:41 -0500

    lint fix

--- src/controllers/spreadsheet.ts
+++ src/controllers/spreadsheet.ts
@@ -6,13 +6,16 @@ import { Registered } from "../db/entity/registered";
 /**
  * Connect to the GoogleSheet document
  */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const getSheet = async (): Promise<any> => {
   console.log("Getting the GoogleSpreadsheet");
   const doc = new GoogleSpreadsheet(process.env.SHEET_ID);
 
   console.log("Authing");
   await doc.useServiceAccountAuth({
+    // eslint-disable-next-line @typescript-eslint/camelcase
     client_email: process.env.GOOGLE_SHEET_CLIENT_EMAIL,
+    // eslint-disable-next-line @typescript-eslint/camelcase
     private_key: process.env.GOOGLE_SHEET_PRIVATE_KEY,
   });
 
@@ -38,6 +41,7 @@ export default async (connection: Connection): Promise<boolean> => {
   console.log(JSON.stringify(process.env));
 
   // Load all deps in async, entries from DB and GoogleSheet info
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const [registrations, doc]: [Registered[], any] = await Promise.all([getRegistrations(connection), getSheet()]);
 
   // First sheet is going to be "latest"
@@ -50,9 +54,9 @@ export default async (connection: Connection): Promise<boolean> => {
   // Convert all Registered rows into GoogleSheet rows
   const rowsToAdd = [];
 
-  for (let r of registrations) {
+  for (const r of registrations) {
     const attending = r.attending ? 1 : 0;
-    for (let user of r.users) {
+    for (const user of r.users) {
       const { rsvpUser } = r;
       rowsToAdd.push({
         ID: r._id,
